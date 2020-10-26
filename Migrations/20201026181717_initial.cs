718dc14d4a603fba5df52a28fa603e59b141088e
Deyvid Lins <deyvidg.lins@gmail.com>
2020-10-26 17:32:34 -0300

    alterações

---
+++ Migrations/20201026181717_initial.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoas.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "varchar(120)", nullable: false),
+                    dataNascimento = table.Column<string>(type: "varchar(20)", nullable: false),
+                    salario = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telefones",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    numero = table.Column<string>(type: "varchar(11)", nullable: false),
+                    tipo = table.Column<string>(type: "varchar(07)", nullable: false),
+                    id_pessoa = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefones", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Telefones_Pessoas_id_pessoa",
+                        column: x => x.id_pessoa,
+                        principalTable: "Pessoas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telefones_id_pessoa",
+                table: "Telefones",
+                column: "id_pessoa");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Telefones");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
