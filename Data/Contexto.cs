146dbe8ffb94ae68f6adb5c32f1e19d529c40523
Deyvid Lins <deyvidg.lins@gmail.com>
2020-10-26 17:16:05 -0300

    Criação da classe contexto

---
+++ Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa>Pessoas { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
