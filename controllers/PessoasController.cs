813e2e04f0e34c9a873a926f690d0a84905793c5
Deyvid Lins <deyvidg.lins@gmail.com>
2020-10-26 17:17:09 -0300

    Controllers Pessoa e Telefone

---
+++ controllers/PessoasController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using CadastroPessoas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Controllers
+{
+    public class PessoasController : Controller
+    {
+
+        private readonly Contexto _contexto;
+
+        public PessoasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+
+        public ViewResult Index(string sortOrder, string searchString)//Página principal 
+        {
+            //Filtragem por nome
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var pessoas = from p in _contexto.Pessoas
+                           select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                pessoas = pessoas.Where(p =>p.nome.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    pessoas = pessoas.OrderByDescending(p => p.nome);
+                    break;
+                case "Date":
+                    pessoas = pessoas.OrderBy(p => p.dataNascimento);
+                    break;
+                case "date_desc":
+                    pessoas = pessoas.OrderByDescending(p => p.salario);
+                    break;
+                default:
+                    pessoas = pessoas.OrderBy(p => p.nome);
+                    break;
+            }
+            var tel = _contexto.Telefones.ToList();
+            ViewBag.tel = tel;
+            
+            return View(pessoas.ToList());
+        }
+    
+        [HttpGet]
+        public IActionResult CriarPessoa()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarPessoa(Pessoa pessoa) //Realiza um create no Banco de dados
+        {
+            if (string.IsNullOrEmpty(pessoa.nome))// Verifica se o campo nome está vazio
+                ModelState.AddModelError("nome", "Campo nome: preenchimento obrigatório.");
+
+           
+            if ((DateTime.Now.Year - pessoa.dataNascimento.Date.Year) < 18)  //Valida se a idade é menor que 18 anos
+                ModelState.AddModelError("dataNascimento", "Campo Nascimento: A idade deve ser maior que 18 anos.");
+
+            if ((DateTime.Now.Year - pessoa.dataNascimento.Date.Year) > 60)  //Valida se a idade é  maior que 60 anos
+                ModelState.AddModelError("dataNascimento", "Campo Nascimento: A idade não pode ser maior que 60 anos.");
+
+            if (ModelState.IsValid)
+            {
+
+                _contexto.Add(pessoa);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else return View(pessoa);
+        }
+        [HttpGet]
+        public IActionResult AtualizarPessoa(int? id)
+        {
+            if (id != null)
+            {
+                Pessoa pessoa = _contexto.Pessoas.Find(id);
+                return View(pessoa);
+            }
+            else return NotFound();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AtualizarPessoa(int? id, Pessoa pessoa)//Atualiza
+        {
+            if (id != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    _contexto.Update(pessoa);
+                    await _contexto.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else return View(pessoa);
+            }
+
+            else return View(pessoa);
+        }
+
+        [HttpGet]
+        public IActionResult ExcluirPessoa(int? id)
+        {
+            if (id != null)
+            {
+                Pessoa pessoa = _contexto.Pessoas.Find(id);
+                return View(pessoa);
+            }
+            else return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ExcluirPessoa(int? id, Pessoa pessoa)//Exclui
+        {
+            if (id != null)
+            {
+                _contexto.Remove(pessoa);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            else return NotFound();
+        }
+      
+        [HttpGet]
+        public IActionResult DetalhesPessoa(int? id)//Detalhes
+        {
+            Pessoa pessoa = _contexto.Pessoas.Find(id);
+
+            if (pessoa == null)
+                return NotFound();
+            else
+                return View(pessoa);
+            
+        }
+    
+    }
+}
+
---
+++ controllers/TelefonesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using CadastroPessoas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Controllers
+{
+    public class TelefonesController : Controller
+    {
+        private readonly Contexto _contexto;
+        public TelefonesController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<IActionResult> Index()
+        {
+           
+            return View(await _contexto.Telefones.ToListAsync());
+        }
+
+        [HttpGet]
+        public ViewResult CriarTelefone(int? id)
+        {
+            Telefone tel = new Telefone();
+
+            if (id != null)
+            {
+                Telefone telefone = _contexto.Telefones.Find(id);
+            }
+
+            var p = _contexto.Pessoas.ToList();
+            ViewBag.tel = p;
+            return View(tel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarTelefone(Telefone telefone) //Realiza um create no Banco de dados
+        {
+            if (telefone.tipo=="Celular")
+            {
+                ViewBag.limite = 11;
+            }else if (telefone.tipo == "Fixo")
+            {
+                ViewBag.limite = 10;
+            }
+            if (string.IsNullOrEmpty(telefone.numero))// Verifica se o campo está vazio
+                ModelState.AddModelError("numero", "Campo numero: preenchimento obrigatório.");
+
+            if (string.IsNullOrEmpty(telefone.tipo))// Verifica se o campo está vazio
+                ModelState.AddModelError("tipo", "Campo tipo: preenchimento obrigatório.");
+
+            if (ModelState.IsValid)
+            {
+
+                _contexto.Add(telefone);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else return View(telefone);
+        }
+        [HttpGet]
+        public IActionResult AtualizarTelefone(int? id)
+        {
+            if (id != null)
+            {
+                Telefone telefone= _contexto.Telefones.Find(id);
+                return View(telefone);
+            }
+            else return NotFound();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AtualizarTelefone(int? id, Telefone telefone)//Atualiza
+        {
+            if (id != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    _contexto.Update(telefone);
+                    await _contexto.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else return View(telefone);
+            }
+
+            else return View(telefone);
+        }
+
+        [HttpGet]
+        public IActionResult ExcluirTelefone(int? id)
+        {
+            if (id != null)
+            {
+                Telefone telefone = _contexto.Telefones.Find(id);
+                return View(telefone);
+            }
+            else return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ExcluirTelefone(int? id, Telefone telefone)//Exclui
+        {
+            if (id != null)
+            {
+                _contexto.Remove(telefone);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            else return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult DetalhesTelefone(int? id, Telefone telefone) //Detalhes
+
+        {
+            var tel = _contexto.Telefones //Faz um select no banco através do _contexto, para verificar se id_pessoa é igual ao que foi passado no parametro int? id
+                  .Where(t => t.id_pessoa == (id)); 
+            
+            foreach (var t in tel) {
+
+                var a = t.id;
+                Telefone tele = _contexto.Telefones.Find(a);
+
+                if (tele == null)
+                    return NotFound();
+                else
+                    return View(tele);
+            }
+            ViewBag.info = "Não existi nenhum contato Telefônico dessa pessoa";
+            return View(telefone);
+
+
+        }
+    }
+}
