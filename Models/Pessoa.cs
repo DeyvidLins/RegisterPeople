c150b285d720bfcb29eba6b14698e2a1bc85301e
Deyvid Lins <deyvidg.lins@gmail.com>
2020-10-26 17:11:22 -0300

    Models Pessoa e Telefone

---
+++ Models/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+    public class Pessoa
+    {
+        public int id { get; set; }
+
+        [Required] //Para o valor não ser Nulo no banco
+        [Display(Name = "Nome")]
+        [Column(TypeName ="varchar(120)")]
+        public string nome { get; set; }
+
+
+        [Display(Name = "Data de Nascimeto:")]
+        public DateTime dataNascimento { get; set; }
+
+        [Display(Name = "Salário")]
+        [Column(TypeName = "money")]
+        public decimal salario { get; set; }
+
+        public List<Telefone> Telefones { get; set; }// Lista de telefone
+
+
+    }
+}
---
+++ Models/Telefone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+   
+    public class Telefone
+    {
+        
+        public int id { get; set; }
+
+        [Required] //Para o valor não ser Nulo no banco
+        [Display(Name = "Número")]
+        [Column(TypeName = "varchar(11)")]
+        public string numero { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo")]
+        [Column(TypeName = "varchar(07)")]
+        public string tipo { get; set; }
+
+        public int id_pessoa { get; set; }
+
+        [ForeignKey(nameof(id_pessoa))]
+        public Pessoa Pessoa { get; set; }//Chave estrangeira
+    }
+}
