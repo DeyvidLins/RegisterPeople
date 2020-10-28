e6fa7519f34d04029372cd975c954ffa0e31ac60
Deyvid Lins <deyvidg.lins@gmail.com>
2020-10-28 11:48:22 -0300

    alterações

---
+++ Migrations/20201028144703_inital04.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoas.Migrations
+{
+    public partial class inital04 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
